61b17500d74d23224b594b2aff0ac3ef2e1f9896
fyvl <mydreamiscybersport@gmail.com>
2021-12-18 21:21:24 +0700

    aaaaaaaaaaaaaaaaaaaaaaaaaa

--- loader.py
+++ loader.py
@@ -2,9 +2,9 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from utils.db.storage import DatabaseManager
 
-from data.config import BOT_TOKEN
+import config
 
-bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 db = DatabaseManager("data/database.db")
